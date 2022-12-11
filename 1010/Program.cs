e8393596e43e71b7eca89b442db7c157b215b79c
Kauet Lima <kauet123@live.com>
2022-12-11 17:04:37 -0300

    Primeiro commit

---
+++ 1010/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, media;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            media = ((a * 2) + (b * 3) + (c * 5)) / (2 + 3 + 5);
+
+            Console.WriteLine("MEDIA = " + media.ToString("F1", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+
+        }
+    }
+}
